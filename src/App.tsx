32d020b047e91d894b46978c8d958c4b490c9b8d
ParkYoungWoong <thesecon@gmail.com>
2024-10-31 13:50:33 +0900

    4일차 4교시 내용.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
+import { useFetchMovies } from '@/hooks/movie.infinite'
 
 export default function App() {
+  const { data, fetchNextPage } = useFetchMovies()
   const [searchText, setSearchText] = useState('')
+
+  function searchMovies() {
+    // ...
+  }
+
   return (
     <>
       <div>
         <input
           value={searchText}
           onChange={e => setSearchText(e.target.value)}
+          onKeyDown={e => e.key === 'Enter' && searchMovies()}
         />
-        <button>검색</button>
+        <button onClick={searchMovies}>검색</button>
+        {data?.pages.map(page => {
+          return page.Search.map(movie => {
+            return <div key={movie.imdbID}>{movie.Title}</div>
+          })
+        })}
+        <button onClick={() => fetchNextPage()}>더보기</button>
       </div>
     </>
   )
--- src/hooks/movie.infinite.ts
+++ src/hooks/movie.infinite.ts
@@ -1,5 +1,11 @@
+import { useState } from 'react'
 import { useInfiniteQuery } from '@tanstack/react-query'
 
+interface ResponseValue {
+  Search: Movie[]
+  totalResults: string
+  Response: string
+}
 interface Movie {
   imdbID: string
   Title: string
@@ -7,12 +13,30 @@ interface Movie {
 }
 
 export const useFetchMovies = () => {
-  return useInfiniteQuery<Movie[]>({
-    // queryKey: ['movies'],
-    // queryFn: async ({ pageParam = 1 }) => {
-    //   const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=avengers`)
-    //   const { Search } = await res.json()
-    //   return Search
-    // }
+  return useInfiniteQuery<ResponseValue>({
+    queryKey: ['movies'],
+    queryFn: async ({ pageParam }) => {
+      const res = await fetch(
+        `https://omdbapi.com/?apikey=7035c60c&s=abcd&page=${pageParam}`
+      )
+      return res.json()
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      const maxPage = Math.ceil(Number.parseInt(pages[0].totalResults, 10) / 10) // 2
+      if (maxPage > pages.length) {
+        return pages.length + 1
+      }
+      return null
+    }
   })
 }
+
+// export const useCustomHook = () => {
+//   return useInfiniteQuery({
+//     queryKey: [],
+//     queryFn: async () => {},
+//     initialPageParam: 1,
+//     getNextPageParam: () => 2
+//   })
+// }
